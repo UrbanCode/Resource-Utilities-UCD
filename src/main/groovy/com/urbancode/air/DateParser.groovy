1938b8ddd3012bfdaeef89079c774bfea4860a53
markroberts0830 <markroberts@Marks-MBP-3.home>
2016-09-23 17:27:22 +0100

    Release formatted for Gradle build

--- src/main/groovy/com/urbancode/air/DateParser.groovy
+++ src/main/groovy/com/urbancode/air/DateParser.groovy
@@ -1,14 +1,10 @@
-/*
-* Licensed Materials - Property of IBM Corp.
-* IBM UrbanCode Build
-* IBM UrbanCode Deploy
-* IBM UrbanCode Release
-* IBM AnthillPro
-* (c) Copyright IBM Corporation 2002, 2014. All Rights Reserved.
-*
-* U.S. Government Users Restricted Rights - Use, duplication or disclosure restricted by
-* GSA ADP Schedule Contract with IBM Corp.
-*/
+/**
+ * © Copyright IBM Corporation 2002, 2016.
+ * This is licensed under the following license.
+ * The Eclipse Public 1.0 License (http://www.eclipse.org/legal/epl-v10.html)
+ * U.S. Government Users Restricted Rights: Use, duplication or disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
+ */
+
 package com.urbancode.air
 
 import java.text.SimpleDateFormat
